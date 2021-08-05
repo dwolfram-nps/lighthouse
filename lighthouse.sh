a4deea226a72a22ff79616ac8046fe0e420da640
Eric Wolfram <derrick.wolfram@northgateps.com>
2021-08-05 09:57:54 +0100

    initial up

--- lighthouse.sh
+++ lighthouse.sh
@@ -6,7 +6,7 @@
 echo "az account show"
 az account show --output jsonc
 
-uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.json
+uri=https://raw.githubusercontent.com/dwolfram-nps/lighthouse/master/necsws.json
 
 echo "uri=$uri"
 echo "az deployment sub create --name lighthouse --template-uri $uri --location westeurope"
